1c30c008da15f3971af38480021acfbd6583770f
vova <gorbenko.vova@gmail.com>
2023-06-21 21:13:36 +0200

    add android build script

---
+++ build_android.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+cmake -DCMAKE_TOOLCHAIN_FILE=~/NDK/build/cmake/android.toolchain.cmake -DANDROID_NDK=~/NDK -DCMAKE_BUILD_TYPE=Release -DANDROID_ABI="armeabi-v7a with NEON" . && cmake --build .
