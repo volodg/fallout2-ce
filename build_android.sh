ad32a2cc28ea399f4b35c747dfce42510803e809
vova <gorbenko.vova@gmail.com>
2023-06-23 18:20:19 +0200

    add rust support

--- build_android.sh
+++ build_android.sh
@@ -5,6 +5,8 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+# brew install openjdk@17
+
 cmake -B build_android \
     -DCMAKE_TOOLCHAIN_FILE=~/NDK/build/cmake/android.toolchain.cmake \
     -DANDROID_NDK=~/NDK -DCMAKE_BUILD_TYPE=Release \
--- build_ios.sh
+++ build_ios.sh
@@ -5,8 +5,12 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+cargo install cargo-lipo
+
 cmake -G Xcode -B build_ios \
+    -DCMAKE_TOOLCHAIN_FILE=./cmake/toolchain/ios.toolchain.cmake \
     -DCMAKE_SYSTEM_NAME=iOS \
+    -DPLATFORM=OS64COMBINED \
     -DCMAKE_Swift_COMPILER_FORCED=true \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0
 
