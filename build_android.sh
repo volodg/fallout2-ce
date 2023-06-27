88c8c169676929b7e1972f59bfd28a5ea2c4d3e4
vova <gorbenko.vova@gmail.com>
2023-06-27 22:32:30 +0200

    fix android build

--- build_android.sh
+++ build_android.sh
@@ -10,5 +10,5 @@ set -o xtrace
 cmake -B build_android \
     -DCMAKE_TOOLCHAIN_FILE=~/NDK/build/cmake/android.toolchain.cmake \
     -DANDROID_NDK=~/NDK -DCMAKE_BUILD_TYPE=Release \
-    -DANDROID_ABI="armeabi-v7a with NEON" . \
+    -DANDROID_ABI="arm64-v8a" . \
     && cmake --build build_android
