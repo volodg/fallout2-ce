e3d98c8b11ce4845eefef7b8d954437c0351ef0a
vova <gorbenko.vova@gmail.com>
2023-07-10 15:14:35 +0200

    migrate file utils into rust

--- rust/src/db.rs
+++ rust/src/db.rs
@@ -1,7 +1,45 @@
-use std::ffi::CString;
+use std::ffi::{c_void, CString};
 use std::ptr::{null, null_mut};
+use std::sync::atomic::{AtomicPtr, Ordering};
 use libc::{c_char, c_int};
 use crate::xfile::{rust_xfile_close, rust_xfile_get_size, rust_xfile_open, xbase_open, XList};
+
+type FileReadProgressHandler = unsafe extern "C" fn();
+
+// Generic file progress report handler.
+//
+// 0x51DEEC
+static G_FILE_READ_PROGRESS_HANDLER: AtomicPtr<c_void> = AtomicPtr::new(null_mut());
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_get_g_file_read_progress_handler() -> FileReadProgressHandler {
+    let result = G_FILE_READ_PROGRESS_HANDLER.load(Ordering::Relaxed);
+    std::mem::transmute(result)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn rust_set_g_file_read_progress_handler(value: FileReadProgressHandler) {
+    G_FILE_READ_PROGRESS_HANDLER.store(std::mem::transmute(value), Ordering::Relaxed)
+}
+
+/*
+
+// Bytes read so far while tracking progress.
+//
+// Once this value reaches [gFileReadProgressChunkSize] the handler is called
+// and this value resets to zero.
+//
+// 0x51DEF0
+static int gFileReadProgressBytesRead = 0;
+
+// The number of bytes to read between calls to progress handler.
+//
+// 0x673040
+static int gFileReadProgressChunkSize;
+
+// 0x673044
+static FileList* gFileListHead;
+ */
 
 #[repr(C)]
 #[allow(dead_code)]
@@ -49,3 +87,14 @@ int dbGetFileSize(const char* filePath, int* sizePtr)
 
 }
  */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rust_get_g_file_read_progress_handler() {
+        let pointer = unsafe { rust_get_g_file_read_progress_handler() };
+        assert_eq!(pointer as *const c_void, null())
+    }
+}
